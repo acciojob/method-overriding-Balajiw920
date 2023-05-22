dd0f550cc6aec99a435ac322897532f5134c1ceb
Balajiw920 <wbalaji2021@gmail.com>
2023-05-22 23:39:07 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,10 @@
 package com.driver;
 
+import com.driver.test.TestCases;
+
 public class Main {
-  
+    public static void main(String[] args) {
+        TestCases testCases = new TestCases();
+        testCases.testMethodOverride();
+    }
 }
